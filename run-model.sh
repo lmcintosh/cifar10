0ffab25df4126b87abf202de202d9f51697997cf
Lane McIntosh <lanemcintosh@gmail.com>
2014-06-04 00:12:11 -0700

    first real model

--- run-model.sh
+++ run-model.sh
@@ -13,6 +13,11 @@ done
 
 TOOLS=../../build/tools
 for file in $(ls param_dir); do
+    #GLOG_alsologtostderr=1 $TOOLS/train_net.bin cifar10_quick_solver_lane01.prototxt
+
+    #reduce learning rate by fctor of 10 after 8 epochs
+    #GLOG_alsologtostderr=1 $TOOLS/train_net.bin cifar10_quick_solver_lr1_lane01.prototxt
+    
     GLOG_alsologtostderr=1 $TOOLS/train_net.bin $file
 done
 
